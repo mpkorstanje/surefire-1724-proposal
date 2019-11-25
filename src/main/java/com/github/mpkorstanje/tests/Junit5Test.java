845cf62555d2d7e81b4606ff570d93c57cfc550c
M.P. Korstanje <rien.korstanje@gmail.com>
2019-11-25 01:19:15 +0100

    A few more tests

--- src/main/java/com/github/mpkorstanje/tests/Junit5Test.java
+++ src/main/java/com/github/mpkorstanje/tests/Junit5Test.java
@@ -6,7 +6,17 @@ public class Junit5Test {
 
 
     @Test
-    void aJunit5Test() {
+    void aJunit5TestA() {
+        System.out.println("Junit5Test was executed");
+    }
+
+    @Test
+    void aJunit5TestB() {
+        System.out.println("Junit5Test was executed");
+    }
+
+    @Test
+    void aJunit5TestC() {
         System.out.println("Junit5Test was executed");
     }
 }
